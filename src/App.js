078bc73af966f827b604652950c45f419c98b56e
Subhan Shaikh <102163333+subhans11@users.noreply.github.com>
2022-09-24 15:28:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -43,13 +43,13 @@ export default function App() {
 }
 
 function Home() {
-  return <h2>Home React App</h2>;
+  return <h2>Home of React App</h2>;
 }
 
 function About() {
-  return <h2>About React App</h2>;
+  return <h2>About of React App</h2>;
 }
 
 function Users() {
-  return <h2>Users React App</h2>;
+  return <h2>Users of React App</h2>;
 }
