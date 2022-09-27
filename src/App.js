99cb2a75419efc3ecbb8b160064d89f3b0130062
Subhan Shaikh <102163333+subhans11@users.noreply.github.com>
2022-09-28 00:38:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ export default function App() {
         <nav>
           <ul>
             <li>
-              <Link to="/">Go to the - Home</Link>
+              <Link to="/">Go to the -- Home</Link>
             </li>
             <li>
-              <Link to="/about">Go to the - About</Link>
+              <Link to="/about">Go to the -- About</Link>
             </li>
             <li>
-              <Link to="/users">Go to the - Users</Link>
+              <Link to="/users">Go to the -- Users</Link>
             </li>
           </ul>
         </nav>
